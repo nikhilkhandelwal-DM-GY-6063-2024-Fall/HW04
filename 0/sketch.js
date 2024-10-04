762839b9f854d46505ea8df5fa74131a9485c947
nikhil0119 <nikhilkhandelwal0119@gmail.com>
2024-10-04 03:07:24 -0400

    added code for the homework 4a

--- 0/sketch.js
+++ 0/sketch.js
@@ -1,12 +1,28 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(235,214,200)
+
+  for (let y = 0; y < height; y += 100) {
+    for (let x = 0; x < width; x += 100) {
+      
+      // Add randomness to the fill color
+      let r = random(0,255);
+      let g = random(0,255);
+      let b = random(0,255);
+      fill(r, g, b);
+      
+      // Randomly vary the size of the circles
+      let randomSizeBig = random(20,80);
+      
+      // Vary the position slightly to break the grid pattern
+      let offsetX = random(-30,30);
+      let offsetY = random(-30,30);
+      
+      // Draw the big circle with random variations
+      ellipse(x + offsetX, y + offsetY, randomSizeBig);
+    }
+  }
 }
 
 function draw() {
-  background(255);
-  fill(0);
-  rect(width / 2.5, height / 4, width / 20, height / 2);
-  rect(width - width / 2.5 - width / 20, height / 4, width / 20, height / 2);
-  rect(width / 2.5 + width / 20, height / 4 - width / 20, width - 2 * width / 2.5 - 2 * width / 20, width / 20);
-  rect(width / 2.5 + width / 20, height / 4 + height / 2, width - 2 * width / 2.5 - 2 * width / 20, width / 20);
 }
